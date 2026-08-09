a0d3132ec6a95473dfec22721356987863ddc32c
InfraOraculoMini <csilvasantin@gmail.com>
2026-08-10 01:00:54 +0200

    chore(release): sellar yokup-rtc v.10.08.2026.r3.00:57

--- yokup-rtc/src/version-stamp.js
+++ yokup-rtc/src/version-stamp.js
@@ -1,12 +1,12 @@
 // Generado por deploy.sh en cada publicación. No editar a mano.
 export const SELLO_WORKER = {
-  "version": "v.10.08.2026.r2.00:39",
-  "deployedAt": "2026-08-09T22:39:11Z",
+  "version": "v.10.08.2026.r3.00:57",
+  "deployedAt": "2026-08-09T22:57:56Z",
   "deployer": "InfraOraculoMini",
   "machine": "MacMini",
   "signature": "InfraOraculoMini · MacMini",
-  "git": "3e66f1ec46326a615938c1364de6bbf821997ac2",
-  "gitShort": "3e66f1e",
-  "gitFull": "3e66f1ec46326a615938c1364de6bbf821997ac2",
+  "git": "480e495cd83f42042004b49a3152f8ca0f6463a4",
+  "gitShort": "480e495",
+  "gitFull": "480e495cd83f42042004b49a3152f8ca0f6463a4",
   "dirty": false
 };
