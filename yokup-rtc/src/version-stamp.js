9830319a160a785d1978acdbd0b946b339169e29
subNeo <csilvasantin@gmail.com>
2026-08-09 23:17:33 +0200

    chore(release): sellar yokup-rtc r8

--- yokup-rtc/src/version-stamp.js
+++ yokup-rtc/src/version-stamp.js
@@ -1,12 +1,12 @@
 // Generado por deploy.sh en cada publicación. No editar a mano.
 export const SELLO_WORKER = {
-  "version": "v.09.08.2026.r7.23:10",
-  "deployedAt": "2026-08-09T21:10:38Z",
-  "deployer": "MorfeoMacMini",
+  "version": "v.09.08.2026.r8.23:16",
+  "deployedAt": "2026-08-09T21:16:54Z",
+  "deployer": "InfraOraculoMacMini",
   "machine": "MacMini",
-  "signature": "MorfeoMacMini · MacMini",
-  "git": "e978069585191c61a66b781587df372366bb5fff",
-  "gitShort": "e978069",
-  "gitFull": "e978069585191c61a66b781587df372366bb5fff",
+  "signature": "InfraOraculoMacMini · MacMini",
+  "git": "c6341a032f219e499c9c08ade0af3cc3f5c25988",
+  "gitShort": "c6341a0",
+  "gitFull": "c6341a032f219e499c9c08ade0af3cc3f5c25988",
   "dirty": false
 };
