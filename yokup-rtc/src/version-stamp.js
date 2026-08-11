8ed9a1a746ae6570179788de2304fba2329cdc32
subNeo <csilvasantin@gmail.com>
2026-08-11 19:03:56 +0200

    chore(release): seal yokup-rtc v.11.08.2026.r3.19:03

--- yokup-rtc/src/version-stamp.js
+++ yokup-rtc/src/version-stamp.js
@@ -1,12 +1,12 @@
 // Generado por deploy.sh en cada publicación. No editar a mano.
 export const SELLO_WORKER = {
-  "version": "v.11.08.2026.r2.18:42",
-  "deployedAt": "2026-08-11T16:42:25Z",
+  "version": "v.11.08.2026.r3.19:03",
+  "deployedAt": "2026-08-11T17:03:21Z",
   "deployer": "InfraOraculoMini",
   "machine": "MacMini",
   "signature": "InfraOraculoMini · MacMini",
-  "git": "0adfd927d130db2a7b1964ee756bef7005d17754",
-  "gitShort": "0adfd92",
-  "gitFull": "0adfd927d130db2a7b1964ee756bef7005d17754",
+  "git": "98bb9d559bfcaf5dce6fefce23a4de4cb3aae360",
+  "gitShort": "98bb9d5",
+  "gitFull": "98bb9d559bfcaf5dce6fefce23a4de4cb3aae360",
   "dirty": false
 };
